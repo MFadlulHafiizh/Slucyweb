3bc122c982762d7ab31b1e8066402f07769e3757
MFadlulHafiizh <muh.hafiizh56@gmail.com>
2021-02-01 15:41:51 +0700

    firebase extension

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -422,6 +422,17 @@
     <!-- Page level custom scripts -->
     <script src="js/demo/chart-area-demo.js"></script>
     <script src="js/demo/chart-pie-demo.js"></script>
+
+    {{-- Firebase --}}
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+    <script src="/__/firebase/8.2.5/firebase-app.js"></script>
+
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+        https://firebase.google.com/docs/web/setup#available-libraries -->
+    <script src="/__/firebase/8.2.5/firebase-analytics.js"></script>
+
+    <!-- Initialize Firebase -->
+    <script src="/__/firebase/init.js"></script>
     <script>
 
     </script>
