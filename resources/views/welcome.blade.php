1884e920d9df5d696d2934e09371e1fb1ffc6a58
MFadlulHafiizh <muh.hafiizh56@gmail.com>
2021-02-03 19:09:49 +0700

    all in landing page, image, transparent navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,45 +17,77 @@
   <!-- Custom fonts for this template -->
   <link href="fontawesome-free/css/all.min.css" rel="stylesheet">
   <link href="simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
 
   <!-- Custom styles for this template -->
   <link href="css/landing-page.min.css" rel="stylesheet">
    <!-- Custom styles for this template-->
    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+   <style>
+      #navbar{
+          overflow: hidden;
+          display: flex;
+          padding-left: 10%;
+          padding-right: 10%;
+          justify-content: space-between;
+          box-shadow: 0 0 15px rgba(0, 0, 0, 1.5);
+          align-items: center;
+          position: fixed;
+          left: 0;
+          top: 0;
+          right: 0;
+          z-index: 1;
+          transition: 0.3s;
+      }
+      .btn-custom{
+        background: transparent;
+        color: white;
+      }
+      .btn-custom:hover{
+        background: white;
+        color: #5f2c82;
+      }
+   </style>
 
 </head>
 
-<body>
+<body style="font-family: 'Montserrat', sans-serif;">
 
   <!-- Navigation -->
-  <nav class="navbar static-top" style="background: linear-gradient(45deg, #0a002c, #0a002c , #5f2c82)">
+  <nav class="navbar navbar-expand-md sticky-top" id="navbar">
     <div class="container">
       <a class="navbar-brand" style="color: rgb(255, 255, 255)" href="#">S-LUCY</a>
-      <a class="btn " style=" color:rgb(255, 255, 255); background: linear-gradient(45deg, #0a002c, #0a002c , #5f2c82); border-radius: 1.5rem; border: transparent;" href="{{ route('login')}}">Sign In</a>
+      <a class="btn btn-custom" href="{{ route('login')}}">Sign In</a>
     </div>
   </nav>
 
   <!-- Masthead -->
   <header class="masthead text-white text-center">
     <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-xl-9 mx-auto">
-          <h1 class="mb-5" style="color: rgb(249, 253, 0)"><img src="img/ss.png">
-            Welcome, this is S-LUCY!</h1>
-        </div>
-        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-          <form>
-            <div class="form-row">
-              <div class="col-12 col-md-9 mb-2 mb-md-0">
-                <input type="email" class="form-control form-control-lg" style="opacity: 80% ;border-radius:1.5rem" placeholder="Enter your email...">
+      <div class="container-fluid.gy-0">
+        <div class="row no-gutters">
+            <div class="col-md-6 offset-md-6">
+              <div class="row no-gutters">
+                <div class="col-md-12" >
+                  <img src="img/ss.png" class="img-fluid">
+                </div>
               </div>
-              <div class="col-12 col-md-3">
-                <a href="{{ route('regist')}}" type="submit" class="btn text-light btn-lg" style="border-radius: 1.5rem;border: transparent;background: linear-gradient(45deg,  #0a002c, #0a002c , #5f2c82)">Sign up!</a>
+              <div class="row no-gutters pb-5 justify-content-end">
+                <div class="col-md-12">
+                  <h2 class="mb-5" style="text-shadow: 2px 2px 8px white; color: white; font-family: 'Montserrat', sans-serif;">
+                    Welcome, this is S-LUCY!
+                  </h2>
+              </div>
+              <div class="row no-gutters pb-5 justify-content-end">
+                <div class="col-md-12">
+                  <p class="mb-5 pl-5 pr-5" style="text-shadow: 1px 1px 5px white ; color: white; font-family: 'Montserrat', sans-serif; text-align:justify;">
+                    Smart Switch (which is actually a light fixture) and the smart plug who named S-LUCY will be controlled by the S-LUCY website.
+                    What can this website do? the first we can set  <i class="fas fa-power-off ml-2 mr-2"></i>  On / Off also we can set <i class="fas fa-hourglass-half ml-2 mr-2"></i> the time to control the two tools through this website.
+                    This project was created in order to fulfill one of the graduate requirements to complete the Telecommunication Engineering Study Program.
+                  </p>
               </div>
             </div>
-          </form>
         </div>
       </div>
     </div>
@@ -86,9 +118,9 @@
         <div class="col-lg-4">
           <div class="features-icons-item mx-auto mb-0 mb-lg-3">
             <div class="features-icons-icon d-flex">
-              <i class="icon-check m-auto" style="color: rgb(255, 255, 255)"></i>
-            </div>
-            <h3 style="color: rgb(255, 255, 255)"">Easy to Use</h3>
+               <i class="fas fa-globe-asia m-auto" style="color: rgb(255, 255, 255)"></i>
+              </div>
+              <h3 style="color: rgb(255, 255, 255)">S-LUCY Website</h3>
             <p class="lead mb-0" style="color: rgb(255, 255, 255)">Ready to use , or customize the S-Lucy!</p>
           </div>
         </div>
@@ -101,23 +133,30 @@
     <div class="container-fluid p-0">
       <div class="row no-gutters">
 
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/smartplug.jpg');"></div>
+        <div class="col-lg-6 order-lg-2 text-white showcase-img">
+          <img src="img/smartplug.jpeg" class="img-fluid w-100 justify-content-center">
+        </div>
         <div class="col-lg-6 text-center order-lg-1 my-auto showcase-text">
           <h2 style="color: rgb(255, 255, 255)">Smart Plug</h2>
           <p class="lead mb-0" style="color: rgb(255, 255, 255)">When you use a plug, you know that that will look great!</p>
         </div>
       </div>
       <div class="row no-gutters">
-        <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/smartfitting.jpeg');"></div>
+        <div class="col-lg-6 text-white showcase-img">
+          <img src="img/smartfitting.jpeg" class="img-fluid w-100 justify-content-center">
+        </div>
         <div class="col-lg-6 text-center my-auto showcase-text">
           <h2 style="color: rgb(255, 255, 255)">Smart Switch</h2>
-          <p class="lead mb-0" style="color: rgb(255, 255, 255)">Newly improved because this is Fitting Lamp, and full of great utility classes, This Is Smart Switch! </p>
+          <p class="lead mb-0 pl-3" style="color: rgb(255, 255, 255)">Newly improved because this is Fitting Lamp, and full of great utility classes, This Is Smart Switch! </p>
         </div>
       </div>
       <div class="row no-gutters">
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-new.jpg');"></div>
+        <div class="col-lg-6 order-lg-2 text-white showcase-img">
+          <img src="img/bg-new.jpg" class="img-fluid w-100 mx-auto" style="display: block">
+        </div>
         <div class="col-lg-6 text-center order-lg-1 my-auto showcase-text">
-          <h2 style="color: rgb(255, 255, 255)">Easy to Use &amp; Customize</h2>
+          <h2 style="color: rgb(255, 255, 255)">S-LUCY Website </h2>
+          <h6 style="color: rgb(255, 255, 255)">Easy to Use &amp; Customize</h6>
           <p class="lead mb-0" style="color: rgb(255, 255, 255)">Beautiful Design easy to understand!</p>
         </div>
       </div>
@@ -185,39 +224,44 @@
         <div class="col-lg-6 h-100 text-center text-lg-left my-auto"style="color: rgb(255, 255, 255)">
           <ul class="list-inline mb-2" style="color: rgb(255, 255, 255)">
             <li class="list-inline-item" style="color: rgb(255, 255, 255)">
-              <a style="color: rgb(255, 255, 255)" href="{{route ('about')}}">About</a>
-            </li>
-            <li class="list-inline-item" style="color: rgb(255, 255, 255)">&sdot;</li>
-            <li class="list-inline-item "style="color: rgb(255, 255, 255)">
-              <a style="color: rgb(255, 255, 255)" href="{{route ('contact')}}">Contact</a>
+              <span class="small mb-4 mb-lg-0" style="color: rgb(255, 255, 255)"><i class="fab fa-telegram fa-fw mr-2" style="color: rgb(255, 255, 255)"></i>Our Telegram : <a href="https://t.me/slucywebsite" class="text-light">slucywebsite</a></span>
             </li>
           </ul>
-          <span class="small mb-4 mb-lg-0" style="color: rgb(255, 255, 255)">&copy; S-LUCY Website {{Carbon\Carbon::now()->format('l, d F Y')}} . All Rights Reserved.</span>
-        </div>
-        <div class="col-lg-6 h-100 text-center text-lg-right my-auto" style="color: rgb(255, 255, 255)">
-          <ul class="list-inline mb-0" style="color:rgb(255, 255, 255);">
-            <li class="list-inline-item mr-3">
-              <a href="https://slucyinfo@gmail.com">
-                <i class="fas fa-envelope fa-2x fa-fw" style="color: rgb(255, 255, 255)" ></i>
-              </a>
+          <ul class="list-inline mb-2" style="color: rgb(255, 255, 255)">
+            <li class="list-inline-item" style="color: rgb(255, 255, 255)">
+              <span class="small mb-4 mb-lg-0" style="color: rgb(255, 255, 255)"><i class="fas fa-envelope fa-fw mr-2" style="color: rgb(255, 255, 255)" ></i>Contact us :  <a href="https://t.me/slucywebsite" class="text-light">slucyinfo@gmail.com</a></span>
             </li>
-            <li class="list-inline-item mr-3">
-              <a href="https://t.me/slucywebsite">
-                <i class="fab fa-telegram fa-2x fa-fw" style="color:rgb(255, 255, 255)";></i>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <i class="fab fa-instagram fa-2x fa-fw" style="color:rgb(255, 255, 255)";></i>
-              </a>
+          </ul>
+          <ul class="list-inline mb-2" style="color: rgb(255, 255, 255)">
+            <li class="list-inline-item" style="color: rgb(255, 255, 255)">
+              <span class="small mb-4 mb-lg-0" style="color: rgb(255, 255, 255)"><i class="fas fa-globe fa-fw mr-2" style="color: rgb(255, 255, 255)" ></i>S-LUCY Website :  <a href="https://t.me/slucywebsite" class="text-light">slucy.com</a></span>
             </li>
           </ul>
         </div>
-      </div>
+        <div class="col-lg-6 h-100 mb-5 pl-5 pr-5 text-center text-lg-right my-auto" style="color: rgb(255, 255, 255)">
+          <span class="small ml-4 mb-lg-0" style="color: rgb(255, 255, 255)">
+            <i class="fas fa-users fa-fw mr-2"></i> Our Team :</span>
+          <ul class="list-inline mb-2" style="color: rgb(255, 255, 255)">
+            <li class="list-inline-item" style="color: rgb(255, 255, 255)">
+              <span class="small mb-4 mb-lg-0" style="color: rgb(255, 255, 255)">Khoerunnisa Cahya Amalia : <a href="https://t.me/Khoerunnisaca" class="text-light">Khoerunnisaca <i class="fab fa-telegram fa-fw ml-2" style="color: rgb(255, 255, 255)" ></i></a></span>
+            </li>
+          </ul>
+          <ul class="list-inline mb-2" style="color: rgb(255, 255, 255)">
+            <li class="list-inline-item" style="color: rgb(255, 255, 255)">
+              <span class="small mb-4 mb-lg-0" style="color: rgb(255, 255, 255)">Lulu Tania :  <a href="https://t.me/lulutania" class="text-light">lulutania</a><i class="fab fa-telegram fa-fw ml-2" style="color: rgb(255, 255, 255)" ></i></span>
+            </li>
+          </ul>
+          <span class="small mb-4 mb-lg-0" style="color: rgb(255, 255, 255)">Almyra Rosedyana :  <a href="https://t.me/almyrarose" class="text-light">almyrarose</a><i class="fab fa-telegram fa-fw ml-2" style="color: rgb(255, 255, 255)" ></i></span>
+        </div>
+        </div>
+        <div class="col-lg-12 mt-3 text-lg-center"style="color: rgb(255, 255, 255)">
+          <span class="small mb-lg-0 " style="color: rgb(255, 255, 255)">&copy; S-LUCY Website {{Carbon\Carbon::now()->format('l, d F Y')}} . All Rights Reserved.</span>
+        </div>
+        </div>
     </div>
   </footer>
   <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="/">
+  <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
   </a>
 
@@ -242,6 +286,24 @@
 
    <!-- Initialize Firebase -->
    <script src="/__/firebase/init.js"></script>
+   <script>
+
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.getElementById("navbar").style.background = " none";
+            document.getElementById("navbar").style.height = "60px";
+            document.getElementById("navbar").style.marginTop = "0";
+            document.getElementById("navbar").style.boxShadow = "0 0 15px rgba(0, 0, 0, 1)";
+
+        } else {
+            document.getElementById("navbar").style.background = "none";
+            document.getElementById("navbar").style.boxShadow = "0 0 15px rgba(0, 0, 0, 1.5)";
+        }
+    }
+</script>
+  
 </body>
 
 </html>
