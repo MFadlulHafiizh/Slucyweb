fcfdd01dfd8ff6e4132f313b80deed6136886826
MFadlulHafiizh <muh.hafiizh56@gmail.com>
2021-02-05 07:35:43 +0700

    clean project

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('product', [MainController::class, 'produk']);
+Route::get('product', [MainController::class, 'product']);
